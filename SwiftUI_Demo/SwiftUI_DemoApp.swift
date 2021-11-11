2666812790bb6f2dee87d52fb332b4773071ffac
ghadeer.elmahdy1 <ghadeer.elmahdy@fixedmea.com>
2021-11-11 23:27:58 +0200

    Initial Commit

---
+++ SwiftUI_Demo/SwiftUI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DemoApp.swift
+//  SwiftUI_Demo
+//
+//  Created by ghadeer.elmahdy on 11/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
